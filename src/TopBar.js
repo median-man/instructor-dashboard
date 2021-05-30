db7db18d464d65f8cb27f3e704b59bae1475a087
John Desrosiers <john.desro@gmail.com>
2021-05-30 03:55:03 -0700

    Update TopBar style

--- src/TopBar.js
+++ src/TopBar.js
@@ -4,11 +4,11 @@ import { useBcs } from "./bcs";
 function TopBar() {
   const { isLoggedIn, signOut } = useBcs();
   return (
-    <header className="navbar navbar-dark bg-dark shadow">
-      <Link className="navbar-brand px-3 col-sm-3" to="/">
+    <header className="navbar navbar-expand navbar-dark bg-dark shadow">
+      <Link className="navbar-brand px-3" to="/">
         Instructor Dashboard
       </Link>
-      <div className="navbar-nav px-3">
+      <div className="navbar-nav px-3 ms-auto">
         <div className="nav-item">
           {isLoggedIn && (
             <button className="nav-link bg-dark btn" onClick={signOut}>
